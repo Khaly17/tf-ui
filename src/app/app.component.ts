0eeff79e35d4c59412af06118752e0603b94ec18
Khaly DIENG <kd.univ@sensor6ty.com>
2025-04-15 15:32:48 +0200

    feat: implement Loader component with styles and tests; update app structure and improve user dashboard and search features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { LoaderComponent } from './layout/loader/loader.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, LoaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'tf-ui';
+  isLoading: boolean = true;
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.isLoading = false; 
+    }, 2000); 
+  }
 }
