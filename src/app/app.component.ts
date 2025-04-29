b1cfcd593312beaf0986213aced9e0154b009df1
Khaly DIENG <kd.univ@sensor6ty.com>
2025-04-29 21:49:38 +0200

    chore: remove unused code and clean up project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tf-ui';
+  title = 'TROQV';
   isLoading: boolean = true;
 
   ngAfterViewInit() {
