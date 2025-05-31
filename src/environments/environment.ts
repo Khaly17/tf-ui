5b2e58847b7c8d88740210093b8e59f8aeb8202b
Khaly DIENG <kd.univ@sensor6ty.com>
2025-05-31 12:58:59 +0200

    fix: update BASE_URL to use the correct production endpoint and comment out localhost URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@
 export const environment = {
     production: false,
     // BASE_URL: "https://tf-back-y0m4.onrender.com" 
-    // BASE_URL: "http://ec2-18-216-105-129.us-east-2.compute.amazonaws.com:5000"
-    BASE_URL: "http://localhost:5000",
+    BASE_URL: "http://ec2-18-216-105-129.us-east-2.compute.amazonaws.com:5000"
+    // BASE_URL: "http://localhost:5000",
   };
